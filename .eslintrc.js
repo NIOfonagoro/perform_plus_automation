9f0c1bfd9e8a975ee75ebb6ec3d338192bc062bc
Nicholas Ofonagoro <nicholas@play-consult.co.uk>
2020-01-08 17:38:28 +0000

    ESLint install

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": ["prettier"],
+    "plugins": ["prettier"],
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        "prettier/prettier": "error"
+    }
+};
